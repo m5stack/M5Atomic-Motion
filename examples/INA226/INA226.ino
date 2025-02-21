28a6bb07914f8705bb90ac3f68cfdc1235b20313
Forairaaaaa <applesyqd@outlook.com>
2025-02-21 14:47:41 +0800

    update example

--- examples/INA226/INA226.ino
+++ examples/INA226/INA226.ino
@@ -13,33 +13,30 @@ void setup() {
     auto cfg = M5.config();
     M5.begin(cfg);
 
-    M5.Display.setTextColor(GREEN);
-    M5.Display.setTextDatum(middle_center);
-    M5.Display.setTextSize(2);
-
     m5::board_t board = M5.getBoard();
+    uint8_t sda, scl = 0;
 
-    uint8_t sda, scl;
-
-    if (board == m5::board_t::board_M5AtomLite) {
+    if (board == m5::board_t::board_M5AtomLite ||
+        board == m5::board_t::board_M5AtomMatrix ||
+        board == m5::board_t::board_M5AtomEcho) {
         sda = 25;
         scl = 21;
-    } else {
+    } else if (board == m5::board_t::board_M5AtomS3 ||
+               board == m5::board_t::board_M5AtomS3R ||
+               board == m5::board_t::board_M5AtomS3Lite ||
+               board == m5::board_t::board_M5AtomS3RExt ||
+               board == m5::board_t::board_M5AtomS3RCam) {
         sda = 38;
         scl = 39;
     }
 
     while (!AtomicMotion.begin(&Wire, M5_ATOMIC_MOTION_I2C_ADDR, sda, scl,
                                100000)) {
-        M5.Display.drawString("Init Fail", M5.Display.width() / 2,
-                              M5.Display.height() / 2);
         printf("Atomic Motion begin failed\n");
         delay(1000);
     }
 
-    printf("Atomic Motion Test\n");
-    M5.Display.drawString("INA226", M5.Display.width() / 2, 25);
-    M5.Display.setTextColor(TFT_YELLOW);
+    printf("Atomic Motion INA226 Test\n");
 }
 
 void loop() {
@@ -48,13 +45,6 @@ void loop() {
 
     printf("voltage: %.2f V\n", voltage);
     printf("current: %.2f A\n", current);
-
-    M5.Display.fillRect(0, 42, M5.Display.width(), M5.Display.height() - 42,
-                        TFT_BLACK);
-    M5.Display.setCursor(30, 65);
-    M5.Display.printf("%.1f V", voltage);
-    M5.Display.setCursor(30, 100);
-    M5.Display.printf("%.1f A", current);
 
     delay(1000);
 }
